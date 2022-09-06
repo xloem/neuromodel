e79ca1e76eef2eb2ae7b816a740272bf06cdecdf
John Doe <john.doe@example.com>
2022-09-06 06:38:37 -0400

    data fetcher

---
+++ 001-schizophrenia-diagnosis/fetch_data.bash
@@ -0,0 +1,4 @@
+cd data
+wget https://repod.icm.edu.pl/api/datasets/251/versions/59/files/download?format=original -O schizophrenia_28_edf.zip
+unzip schizophrenia_28_edf.zip
+sha256sum -c sha256sums
